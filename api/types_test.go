802caf1ab68aeee0a28297f2d2158870aa863649
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2024-02-15 20:12:51 +0100

    added tests

--- api/types_test.go
+++ api/types_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestKeepAliveParsing(t *testing.T) {
+func TestKeepAliveParsingFromStruct(t *testing.T) {
 	tests := []struct {
 		name string
 		req  *ChatRequest
@@ -40,6 +40,44 @@ func TestKeepAliveParsing(t *testing.T) {
 
 			var dec ChatRequest
 			err = json.Unmarshal([]byte(ser), &dec)
+			require.NoError(t, err)
+
+			assert.Equal(t, test.exp, dec.KeepAlive)
+		})
+	}
+}
+func TestKeepAliveParsingFromJSON(t *testing.T) {
+	tests := []struct {
+		name string
+		req  string
+		exp  *Duration
+	}{
+		{
+			name: "Positive Integer",
+			req:  `{ "keep_alive": 42 }`,
+			exp:  &Duration{42 * time.Second},
+		},
+		{
+			name: "Positive Integer String",
+			req:  `{ "keep_alive": "42m" }`,
+			exp:  &Duration{42 * time.Minute},
+		},
+		{
+			name: "Negative Integer",
+			req:  `{ "keep_alive": -1 }`,
+			exp:  &Duration{math.MaxInt64},
+		},
+		{
+			name: "Negative Integer String",
+			req:  `{ "keep_alive": "-1m" }`,
+			exp:  &Duration{math.MaxInt64},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var dec ChatRequest
+			err := json.Unmarshal([]byte(test.req), &dec)
 			require.NoError(t, err)
 
 			assert.Equal(t, test.exp, dec.KeepAlive)
